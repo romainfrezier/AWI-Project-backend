cf4740bba4810ee3faede43c5fc72a0ae0508c51
romainfrezier <romainfrezier@gmail.com>
2023-02-01 10:39:51 +0100

    Change region to eur-west1 (Belgium)

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-exports.api = functions.region("europe-west2").https.onRequest(app);
+exports.api = functions.region("europe-west1").https.onRequest(app);
