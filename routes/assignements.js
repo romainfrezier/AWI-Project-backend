a5dd0c36d9d952fb80d6a1de93543fc771b053e8
Robin Hermet <robin.hermet48@gmail.com>
2023-02-07 17:54:08 +0100

    Modifications on routes

--- routes/assignements.js
+++ routes/assignements.js
@@ -8,9 +8,9 @@ router.put('/:id',assignementsCtrl.modifyAssignment);
 router.delete('/:id', assignementsCtrl.deleteAssignment);
 router.get('/', assignementsCtrl.getAllAssignments);
 router.get('/dates',assignementsCtrl.getAllStartingDates);
-router.get('dates/:date_deb',assignementsCtrl.getVolunteersWithDate);
+router.get('/dates/:date_deb',assignementsCtrl.getVolunteersWithDate);
 router.get('/areas',assignementsCtrl.getAllAreas);
-router.get('areas/:zone',assignementsCtrl.getVolunteersWithArea);
+router.get('/areas/:zone',assignementsCtrl.getVolunteersWithArea);
 router.get('/:id', assignementsCtrl.getOneAssignment);
 
 
