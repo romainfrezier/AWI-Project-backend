295ac8553913418128e63dca1780a945bcf1fe55
Robin Hermet <robin.hermet48@gmail.com>
2023-02-07 17:50:41 +0100

    Modifications on routes

--- routes/assignements.js
+++ routes/assignements.js
@@ -6,11 +6,12 @@ const assignementsCtrl = require('../controllers/assignments');
 router.post('/',assignementsCtrl.createAssignment);
 router.put('/:id',assignementsCtrl.modifyAssignment);
 router.delete('/:id', assignementsCtrl.deleteAssignment);
-router.get('/:id', assignementsCtrl.getOneAssignment);
 router.get('/', assignementsCtrl.getAllAssignments);
 router.get('/dates',assignementsCtrl.getAllStartingDates);
 router.get('dates/:date_deb',assignementsCtrl.getVolunteersWithDate);
 router.get('/areas',assignementsCtrl.getAllAreas);
 router.get('areas/:zone',assignementsCtrl.getVolunteersWithArea);
+router.get('/:id', assignementsCtrl.getOneAssignment);
+
 
 module.exports = router;
