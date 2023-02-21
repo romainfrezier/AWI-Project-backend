4f31a7040eb927f5460e7fbcde6f93c5cb5bda1c
romainfrezier <romainfrezier@gmail.com>
2023-02-21 09:32:55 +0100

    Cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const assignmentRoutes = require('./routes/assignements');
 const volunteerRoutes = require('./routes/volunteers');
@@ -11,16 +12,10 @@ mongoose.connect(`mongodb+srv://${process.env.DB_URL}`, { useNewUrlParser: true,
     .then(() => console.log('Connecion to MongoDB successful!'))
     .catch(() => console.log('Connecion to MongoDB failed!'));
 
-const app = express();    
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/assignments', assignmentRoutes);
 app.use('/volunteers', volunteerRoutes);
